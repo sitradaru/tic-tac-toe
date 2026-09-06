7d36484e2b1359ef9a74d1acfc38dddbb6721855
Sitra <sitradaru@gmail.com>
2026-09-06 18:33:29 +0300

    feat: add game board state management

--- script.js
+++ script.js
@@ -2,11 +2,41 @@ function cell() {
     let value = "";
 
     const getValue = () => value; 
-    const setValue = (playerMark) => {
-        if(!value) {
-            value = playerMark;
+    const setValue = (playerMark) => value = playerMark;
+
+    return { getValue, setValue }
+}
+
+function gameBoard() {
+    const board = [];
+    const row = 3;
+    const column = 3;
+
+    for(let i=0; i<row; i++) {
+        board[i] = [];
+        for(let j=0; j<column; j++) {
+            board[i].push(cell())
         }
     }
 
-    return { getValue, setValue }
+    const getBoard = () => board;
+
+    const updateBoard = (row, column, playerMark) => {
+        const targetCell = board[row][column];
+        
+        if(targetCell.getValue()==="") {
+            targetCell.setValue(playerMark);
+        }
+    }
+
+    const printValues = () => {
+        const cellValues = board.map(row => {
+            return row.map(column => column.getValue())
+        }
+        )
+
+        return cellValues;
+    }
+
+    return { getBoard, updateBoard, printValues }
 }
